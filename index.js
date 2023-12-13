d18d020343e55cc81cb3669c92734668b6c52cd0
nrasch86 <145396887+nrasch86@users.noreply.github.com>
2023-12-13 18:18:07 -0500

    Added questions to index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const questions = [
+
+    {
+        type: "input",
+        name: "shape",
+        message: "What shape would you like your logo to be?",
+        choices: ["Square", "Circle", "Triangle"],
+    },
+    {
+        type: "input",
+        name: "Shape Color",
+        message: "What color would you like your shape to be?"
+    },
+    {
+        type: "input",
+        name: "text",
+        message: "Type (3) characters for logo name"
+    },
+    {
+        type: "input",
+        name: "text color",
+        message: "What color would you like your text to be?"
+    },
+];
